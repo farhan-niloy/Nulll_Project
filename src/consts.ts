cb1c6b494d6788e0c1029a8d1b339518353e8636
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-06-09 01:29:11 +0600

    update website infos

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Null Void Of Thoughts'
+export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const HOMEPAGE_URL = 'https://nulll.me/'
