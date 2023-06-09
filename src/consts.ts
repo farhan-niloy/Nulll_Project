ce240205fd44ee0a7da105b7a1ac39af13ff4421
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-06-09 12:50:13 +0600

    update website info

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Null Void Of Thoughts'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_DESCRIPTION =
+  'Here I question existence, talk about philosophy & physics'
 export const HOMEPAGE_URL = 'https://nulll.me/'
